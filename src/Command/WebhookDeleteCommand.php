b1ebbc49f093304e21f54e8339888b9bc9fa238e
Michael <Mihail.Kezin@gmail.com>
2025-11-07 17:46:41 +0200

    Chore: Fix symfony/console deprecations (#3)

--- src/Command/WebhookDeleteCommand.php
+++ src/Command/WebhookDeleteCommand.php
@@ -7,27 +7,22 @@ namespace Luzrain\TelegramBotBundle\Command;
 use Luzrain\TelegramBotApi\BotApi;
 use Luzrain\TelegramBotApi\Exception\TelegramApiException;
 use Luzrain\TelegramBotApi\Method;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(
+    name: 'telegram:webhook:delete',
+    description: 'Remove webhook integration',
+)]
 final class WebhookDeleteCommand extends Command
 {
     public function __construct(
         private BotApi $botApi,
     ) {
         parent::__construct();
-    }
-
-    public static function getDefaultName(): string
-    {
-        return 'telegram:webhook:delete';
-    }
-
-    public static function getDefaultDescription(): string
-    {
-        return 'Remove webhook integration';
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
